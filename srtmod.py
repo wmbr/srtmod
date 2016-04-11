0a49e2b5f5c70035eaa835b749f3beb91be411b8
Malte Renken <malte.renken@fu-berlin.de>
2016-04-11 16:18:53 +0200

    Code cleanup. Print errors to stderr. Look for --help argument.

--- srtmod.py
+++ srtmod.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 from datetime import datetime, timedelta
-from functools import partial
-from operator import add, mul
 import os
 import sys
 import textwrap
@@ -31,8 +29,7 @@ class Operation:
 			time = timedelta_parse(time)
 			return (delta,time)
 		except ValueError:
-			pass
-		raise ValueError("Invalid operation: '{}'".format(opstring))
+			raise ValueError("Invalid operation: '{}'".format(opstring))
 
 	def __call__(self, time):
 		return time + self.delta0 + self.ddelta*time
@@ -80,8 +77,7 @@ def process_line(line, op):
 	return start + SEPARATOR + end + "\n"
 
 
-def print_help():
-	print(textwrap.dedent(
+HELPTEXT = textwrap.dedent(
 	"""
 	Usage: {program} [<delta>][@<time>] [<delta>][@<time>]
 		delta:  One of '+number', '-number' where number is the amount of milliseconds to move the subtitles to the back. If omitted, "+0" is assumed.
@@ -93,23 +89,27 @@ def print_help():
 	Examples:
 		Move subs one second to the front everywhere: {program} -1000 < subtitles.srt
 		Move subs 500ms to the front at 10min but keep the timing at 5min (speeding them up in result): {program} @5:00 -500@10:00 < subtitles.srt
-	""".format(program=os.path.basename(sys.argv[0]))))
+	""".format(program=os.path.basename(sys.argv[0])))
 
 
 def main():
 	args = sys.argv[1:]
+	if len(args) < 1 or len(args) > 2 or args[0] in ["--help", "-help", "-h", "-?"]:
+		print(HELPTEXT)
+		return
 	if len(args) == 1:
 		args.append(args[0])
-	if len(args) == 2:
-		try:
-			op = Operation(args[0], args[1])
-			for line in sys.stdin:
+		
+	try:
+		op = Operation(args[0], args[1])
+		for line in sys.stdin:
+			try:
 				sys.stdout.write(process_line(line, op))
-		except ValueError as v:
-			print(v)
-			print_help()
-	else:
-		print_help()
+			except ValueError:
+				raise ValueError("Invalid input line: {}".format(line))
+	except ValueError as v:
+		print(v, file=sys.stderr)
+		sys.exit(1)
 
-
-main()
+if __name__ == "__main__":
+	main()
